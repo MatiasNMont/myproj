d6fe592305be215945f897b84b46863757deee7a
MatiasNMont <nicolasmontivero.nw273@gmail.com>
2018-12-27 09:22:34 -0300

    Modifique todo

--- Program.py
+++ Program.py
@@ -1,8 +1,13 @@
-
-def function_Define(rocks):
-	rocka=[]
-	n = int(raw_input("Escriba Cantidad:")
-	for i in range(n):
+from random import randint
+n=0
+while n < 3:
+	number = randint(0,10)
+	number_user = int(raw_input("Ingrese Number:"))
+	if number == number_user:
+		break
+	else:
+		n+=1
+else:
+	print "Perdiste"
 	
 
-print ("HOla Mundo")
